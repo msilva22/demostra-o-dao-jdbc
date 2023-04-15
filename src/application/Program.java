605fa14d4343c7bf5a31f8c3e4a4c6d9781581c3
Moisés Silva <moises.ambiental@hotmail.com>
2023-04-15 20:38:43 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -34,6 +35,16 @@ public class Program {
 		}
 		
 
+		System.out.println("\n=== TESTE 4: seller insert ====");
+		Seller newSeller  =  new Seller(null, "Greg", "greg@gmail.com",new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted ! new id = " + newSeller.getId());
+		
+		System.out.println("\n=== TESTE 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Upadate completed");
 	}
 
 }
